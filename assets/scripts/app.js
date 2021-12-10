0efdccff0e8f393b5daa7cfaa90133e15d4118aa
Immanuel Currah <immanuelcurrah@Immanuels-MBP.lan>
2021-12-10 14:43:01 -0500

    made html css and js files

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+async function dataHandler() {
+  const url = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+  let dataContent = await axios.get(url);
+  console.log(dataContent);
+}
+
+dataHandler();
