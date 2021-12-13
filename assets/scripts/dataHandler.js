934d957978148652cf60574c1dd0fe9b4b09672d
Immanuel Currah <immanuelcurrah@Immanuels-MBP.lan>
2021-12-13 11:34:27 -0500

    got images to show on screen

--- assets/scripts/dataHandler.js
+++ assets/scripts/dataHandler.js
@@ -1,18 +1,20 @@
 const randomizerBtn = document.querySelector(".random-btn");
 const firstCardSpecs = document.querySelector(".first-card");
 const secondCardSpecs = document.querySelector(".second-card");
+const firstImage = document.createElement("img");
+const secondImage = document.createElement("img");
 
 async function dataHandler() {
   const url =
     "https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Normal Monster&race=Dragon";
   let dataContent = await axios.get(url);
   let monsterDataBase = dataContent.data.data;
-  // console.log(monsterDataBase);
+  console.log(monsterDataBase);
   monsterDataBaseHandler(monsterDataBase);
 }
 
-const firstMonsterSpecs = (name, attack, defence) => {
-  console.log(`second monster: ${name}, atk: ${attack}, def: ${defence}`);
+const firstMonsterSpecs = (name, attack, defence, image) => {
+  firstCardSpecs.innerHTML = " ";
   let nameOfMonster = document.createElement("h2");
   nameOfMonster.innerText = name;
   firstCardSpecs.appendChild(nameOfMonster);
@@ -24,10 +26,15 @@ const firstMonsterSpecs = (name, attack, defence) => {
 
   firstCardSpecs.appendChild(atk);
   firstCardSpecs.appendChild(def);
+
+  firstImage.src = image;
+  firstImage.alt = `this is a picture of ${name}`;
+
+  firstCardSpecs.appendChild(firstImage);
 };
 
-const secondMonsterSpecs = (name, attack, defence) => {
-  console.log(`first monster: ${name}, atk: ${attack}, def: ${defence}`);
+const secondMonsterSpecs = (name, attack, defence, image) => {
+  secondCardSpecs.innerHTML = " ";
   let nameOfMonster = document.createElement("h2");
   nameOfMonster.innerText = name;
   secondCardSpecs.appendChild(nameOfMonster);
@@ -39,6 +46,11 @@ const secondMonsterSpecs = (name, attack, defence) => {
 
   secondCardSpecs.appendChild(atk);
   secondCardSpecs.appendChild(def);
+
+  secondImage.src = image;
+  secondImage.alt = `this is a picture of ${name}`;
+
+  secondCardSpecs.appendChild(secondImage);
 };
 
 const monsterDataBaseHandler = (dataBase) => {
@@ -47,14 +59,16 @@ const monsterDataBaseHandler = (dataBase) => {
   let name = firstRandomMonster.name;
   let attack = firstRandomMonster.atk;
   let defence = firstRandomMonster.def;
+  let image = firstRandomMonster.card_images[0].image_url_small;
 
   const secondRandomMonster = dataBase[Math.floor(Math.random() * 10)];
   let nameTwo = secondRandomMonster.name;
   let attackTwo = secondRandomMonster.atk;
   let defenceTwo = secondRandomMonster.def;
+  let imageTwo = secondRandomMonster.card_images[0].image_url_small;
 
-  secondMonsterSpecs(nameTwo, attackTwo, defenceTwo);
-  firstMonsterSpecs(name, attack, defence);
+  secondMonsterSpecs(nameTwo, attackTwo, defenceTwo, imageTwo);
+  firstMonsterSpecs(name, attack, defence, image);
 };
 
 const app = (dataBase) => {
